16012da4476a2b3a7bb5649d3b22756bff2b5b53
Abdeljabar AIT BELKHALIL <abdeljabar.aitbelkhalil@cgi.com>
2023-05-10 21:43:47 +0200

    ADD session API

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,6 +1,31 @@
-import React from "react";
-const AuthContext = React.createContext(
-    { isLoggedIn: false }
-);
+import React, { useEffect, useState } from "react";
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogout: () => { },
+    onLogin: (email, password) => { }
+});
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+    return <AuthContext.Provider value={{ isLoggedIn: false, onLogin: loginHandler, onLogout: logoutHandler }}>
+        {props.children}
+    </AuthContext.Provider>
+}
 
 export default AuthContext;
